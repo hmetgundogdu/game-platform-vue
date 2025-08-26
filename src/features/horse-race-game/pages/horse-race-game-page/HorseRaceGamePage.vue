abd6050ff5c0ae4a7cfd800b68bf10679d935013
Ahmet Gündoğdu <hmetgundogdu@gmail.com>
2025-08-26 03:04:18 +0300

    trigger generate dummy race infos while horse page before mount

--- src/features/horse-race-game/pages/horse-race-game-page/HorseRaceGamePage.vue
+++ src/features/horse-race-game/pages/horse-race-game-page/HorseRaceGamePage.vue
@@ -1,8 +1,19 @@
 <script setup>
+import { useStore } from '@/app/store';
+import { onBeforeMount, onMounted } from 'vue';
+import { HorseRaceGameActionPx } from '../../store/action-types';
+
 import HorseRaceGameHeader from './components/horse-race-game-header/HorseRaceGameHeader.vue';
 import HorseRaceGameArea from './components/horse-race-game-area/HorseRaceGameArea.vue';
+
 import '../../styles/game-font-definition.css';
 import '../../styles/game-scroll-design.css';
+
+const store = useStore();
+
+onBeforeMount(() => {
+    store.dispatch(HorseRaceGameActionPx.GENERATE);
+})
 
 </script>
 
